14785ab0b331b1c52eaae2a585853c0b9963361d
abelghalem <abelghalem@yahoo.fr>
2013-12-06 17:35:31 +0100

    Ajout des deux points pour le readFile

--- model/server.js
+++ model/server.js
@@ -29,7 +29,7 @@ oCase_BD.Initialiser();
 var server = http.createServer(function (req, res) {
     console.log("SERVEUR : initialisation du serveur");
 
-    fs.readFile('./view/accueil.ejs', 'utf-8', function (error, content) {
+    fs.readFile('../view/accueil.ejs', 'utf-8', function (error, content) {
         res.writeHead(200, {
             "Content-Type": "text/html"
         });
