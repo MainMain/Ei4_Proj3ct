0088d709419f29936eb6d1f6ac5dafac6b68fcec
Mainmain <psyko469@hotmail.fr>
2013-12-07 13:07:34 +0100

    Implémentation deposer - sequiper - se desequiper Manque juste l'interface client pour finir les tests !

--- model/object/Personnage.js
+++ model/object/Personnage.js
@@ -11,15 +11,15 @@ var Personnage = (function() {
 	// --- ATTRIBUTS DE CLASSE ---
 	Personnage.id;
 	Personnage.ptSante;
-	Personnage.ptSanteMax ;
+	Personnage.ptSanteMax;
 	Personnage.ptActions;
 	Personnage.ptActionsMax;
 	Personnage.ptDeplacement;
 	Personnage.ptDeplacementMax;
 	Personnage.poidsMax;
 	Personnage.idSalleEnCours;
-	Personnage.idArmeEquipee;
-	Personnage.idArmureEquipee;
+	Personnage.armeEquipee;
+	Personnage.armureEquipee;
 	Personnage.sacADos;
 
 	// --- ENUMERATIONS DE CLASSE ---
@@ -47,15 +47,15 @@ var Personnage = (function() {
 		this.ptDeplacementMax = ptDeplacementMax;
 		this.poidsMax = poidsMax;
 		this.idSalleEnCours = idSalleEnCours;
-		this.idArmeEquipee = armeEquipee;
-		this.idArmureEquipee = armureEquipee;
+		this.armeEquipee = armeEquipee;
+		this.armureEquipee = armureEquipee;
 		this.sacADos = sacADos;
 		console.log("PERSONNAGE : Nouveau personnage crée ");
 	}
 
 	// --- METHODES D'INSTANCE
 	Personnage.prototype = {
-		
+
 		/**
 		 * FONCTION DE DEPLACEMENT D'UN PERSONNAGE return : id de la salle si ok
 		 * erreur : -1 si déplacement impossible (pas de case dans la direction)
@@ -96,42 +96,121 @@ var Personnage = (function() {
 		 * @method ajouterAuSac
 		 */
 		ajouterAuSac : function(item) {
-			//this.logAfficherSacADos();
 			this.sacADos.push(item);
-			//this.logAfficherSacADos();
-			console.log("PERSONNAGE : ajout de l'item " + item.nom + " au personnage ");
+			console.log("PERSONNAGE : ajout de l'item " + item.nom
+					+ " au personnage " + this.id);
 		},
 
 		/**
+		 * FONCTION DEPOSER UN OBJET
+		 * 
+		 * @method supprimerDuSac
+		 */
+		supprimerDuSac : function(item) {
+			console.log("PERSONNAGE : suppression de l'item " + item.nom
+					+ " du personnage " + this.id);
+			this.logAfficherSacADos();
+			var index;
+			for (var i = 0; i < this.sacADos.length; i++) {
+				if (this.sacADos[i].id == item.id) {
+					index = i;
+					break;
+				}
+			}
+			//index = index - 1;
+			console.log("CASE : DEBUG index : " + index);
+			this.sacADos.splice(i, 1);
+			this.logAfficherSacADos();
+		},
+
+		/*
+		 * FONCTION POUR AFFICHER DANS LA CONSOLE LA LISTE DES OBJETS DE DU SAC
+		 */
+		logAfficherSacADos : function() {
+			console.log("PERSONNAGE : ****** AFFICHAGE OBJET PERSONNAGE :  "
+					+ this.sacADos.length - 1 + " *********");
+			for (var i = 0; i < this.sacADos.length; i++) {
+				console.log("PERSONNAGE : Objet id = " + this.sacADos[i].id
+						+ " - " + this.sacADos[i].nom);
+			}
+			console.log("PERSONNAGE : *********************************");
+		},
+
+		/**
+		 * FONCTION POUR REVOYER SI UN ITEM EST BIEN DANS LE SAC return :true si
+		 * objet est dans la salle, sinon false
+		 * 
+		 * @method existItemInSac
+		 */
+		existItemInSac : function(item) {
+			if (this.sacADos.indexOf(item) != -1) {
+				console.log("CASE : L'item (" + item.id + " - " + item.nom
+						+ ") est bien dans le sac  du perso " + this.id);
+				return true;
+			} else {
+				console.log("CASE : WARNING : L'item (" + item.id + " - "
+						+ item.nom + ") n'est pas dans le sac du perso  "
+						+ this.id);
+				return false;
+			}
+		},
+
+		/**
+		 * FONCTION POUR S'EQUIPER D'UN ITEM
+		 * return 1 si ok
+		 * erreur : -1 si déja une arme équipée
+		 * erreur : -2 si déja une arme équipée
+		 * erreur : -3 si ni une arme, ni une armure
+		 */
+		sEquiperDunItem : function(item) 
+		{
+			 if (item.type == 1)
+				{
+					if (this.armeEquipee != null)
+						return -1;
+					else
+						this.armeEquipee ==item;
+				}
+				
+			else if (item.type == 2)
+				{
+					if (this.armureEquipee != null)
+						return -2;
+					else
+						this.armureEquipee == item;
+				}
+			else 
+				return -3;
+		},
+		/**
+		 * FONCTION POUR SE DEQUIPER D'UN ITEM
+		 * 
+		 */
+		sDesequiperDunItem : function(item) 
+		{
+			if (item.type == 1) this.armeEquipee = null;
+			else if (item.type == 2) this.armureEquipee = null;
+		},
+		/**
 		 * FONCTION QUI RENVOI LE POIDS DU SAC A DOS
-		 * item
 		 * 
 		 * @method getPoidsSac
 		 */
 		getPoidsSac : function() {
 			var poids = 0;
-			console.log("PERSONNAGE : DEBUG : nombre d'objets dans sac " + this.sacADos.length);
+			console.log("PERSONNAGE : DEBUG : nombre d'objets dans sac "
+					+ this.sacADos.length);
 			// calcule le poids du sac + poids item
 			var i = 0;
-			for (i=0;i<this.length;i++){
-			    poids = poids + curItem.poids;
+			for (i = 0; i < this.sacADos.length; i++) {
+				poids = poids + this.sacADos[i].poids;
 			}
+			//if (armeEquipee != null) poids += armeEquipee.poids;
+			//if (armureEquipee != null) poids += armeEquipee.poids;
 			console.log("PERSONNAGE : Calcul du poids total du sac : " + poids);
 			return poids;
 		},
-		
-		/*
-		 * FONCTION POUR AFFICHER DANS LA CONSOLE LA LISTE DES OBJETS DE DU SAC
-		 */
-		logAfficherSacADos : function()
-		{
-			console.log("PERSONNAGE : ****** AFFICHAGE OBJET PERSONNAGE :  " + this.sacADos.length-1 + " *********");
-			for (var i=0; i<this.sacADos.length; i++) {
-				   console.log("PERSONNAGE : Objet id = " + this.sacADos[i].id + " - " + this.sacADos[i].nom);
-				}
-			console.log("PERSONNAGE : *********************************");
-		},
-		
+
 		/**
 		 * Retourne la sante du personnage
 		 * 
@@ -168,8 +247,23 @@ var Personnage = (function() {
 			return this.idSalleEnCours;
 		},
 
-	};
+		/**
+		 * ACTUALISE LE PERSONNAGE. DOIT ÊTRE DÉCLENCHÉE QUAND LE SERVEUR RELÈVE
+		 * UN ... ... CHANGEMENT FAITE PAR UN AUTRE JOUEUR
+		 */
+		actualiser : function() {
+			// chercher infos dans la BD
+		},
 
+		/**
+		 * UPDATE EN BD LE PERSONNAGE. DOIT ÊTRE DÉCLENCHÉE QUAND LE SERVEUR
+		 * RELÈVE UN ... ... CHANGEMENT FAITE PAR LE JOUEUR EN COURS
+		 */
+		update : function() {
+			// écrire infos dans la BD
+		},
+
+	};
 	// On pense à retourner le constructeur (afin de pouvoir construire des
 	// instances, sinon tout
 	// le code de notre classe serait inutile car non visible depuis
--- model/server.js
+++ model/server.js
@@ -8,10 +8,9 @@ var express = require('express');
 var oPersonnage = require('./object/Personnage');
 var oCarte = require('./object/Carte');
 var fs = require('fs');
-//var oCase = require('./object/Case');
-//var oCarte = require('./object/Carte');
 var oCase_BD = require('../persistance/Case_BD');
 var oItem_BD = require('../persistance/Item_BD');
+
 
 //var eventjeu = new EventEmitter();
 
@@ -25,43 +24,47 @@ oCarte.Initialiser(3, 4);
 oCase_BD.Initialiser();
 
 
-// Chargement du fichier index.html affiché au client
+/*
+ * LANCEMENT DU SERVEUR ET ENVOI DE LA PAGE D'ACCUEIL
+ */
 var server = http.createServer(function (req, res) {
     console.log("SERVEUR : initialisation du serveur");
 
-    fs.readFile('./view/accueil.ejs', 'utf-8', function (error, content) {
+    fs.readFile('../view/accueil.ejs', 'utf-8', function (error, content) {
         res.writeHead(200, {
             "Content-Type": "text/html"
         });
         res.end(content);
     });
 });
-
-// Chargement de socket.io
+//Chargement de socket.io
 var io = require('socket.io').listen(server, {
     log: false
 });
+
+
+/*
+ * INITIALISATION DU PERSONNAGE
+ */
 var sacADos = [oItem_BD.GetItemById(9), oItem_BD.GetItemById(10), oItem_BD.GetItemById(11)];
-
-
 var myPerso = new oPersonnage(10, 100, 100, 20, 25, 10,
-    15, 100, 0, 5, 6, sacADos);
+    15, 100, 0, null, null, sacADos);
 
 
 /*********** EVENEMENTS LORS DE RECEPETION D'UNE COMMUNICATION CLIENT -> SERVEUR **************/
 /*
-* CONNEXION D'UN CLIENT
-*/
+ * CONNEXION D'UN CLIENT
+ */
 io.sockets.on('connection', function (socket) {
     console.log('SERVER : Un client est connecté !');
     socket.emit('MESSAGE_SC', "Salle du perso : " + myPerso.getIdSalleEnCours());
 
-    /*
-* RECEPTION D'UNE DEMANDE DE DEPLACEMENT VERS UNE DIRECTION DONNEE
-* Renvoi la case avec MOVE_PERSONNAGE_SC
-* Si erreur : renvoi "ERREUR_MOVE" si impossible de bouger
-* Si erreur : renvoi "ERREUR_CASE" si erreur de case
-*/
+    /*****************************************************************************************************
+     * RECEPTION D'UNE DEMANDE DE DEPLACEMENT VERS UNE DIRECTION DONNEE
+     * Renvoi la case avec MOVE_PERSONNAGE_SC
+     * Si erreur : renvoi "ERREUR_MOVE" si impossible de bouger
+     * Si erreur : renvoi "ERREUR_CASE" si erreur de case
+     */
     socket.on('MOVE_PERSONNAGE_CS', function (move) {
         // log
         console.log('SERVER : Déplacement du personnage demandé : ' + move);
@@ -84,60 +87,90 @@ io.sockets.on('connection', function (socket) {
             socket.emit('MOVE_PERSONNAGE_SC', "ERREUR_MOVE");
         }
     });
-
-    /*
-* RECEPTION D'UNE DEMANDE D'INFORMATION SUR UNE CASE
-* Renvoi la case avec INFO_CASE_SC
-* Si erreur : renvoi NULL
-*/
-    socket.on('INFO_CASE_CS', function () {
-        // log
-        console.log('SERVER : Infos case demandées ! id : ' + myPerso.idSalleEnCours);
-        // récupère la salle en cours
-        var currentCase = oCase_BD.GetCaseById(myPerso.idSalleEnCours);
-        // return selon la valeur de retour
-        if (currentCase == null)
-            socket.emit('INFO_CASE_SC', "ERREUR_CASE");
-        else
-            socket.emit('INFO_CASE_SC', currentCase);
-    });
-
-
-
-
-    /*
-* RECEPTION D'UNE DEMANDE POUR RAMASSER OU DEPOSER UN OBJET
-* return poidsTotal si ok
-* erreur : -1 si poids insufisant
-* erreur : -2 si objet n'est pas dans la case
-* erreur : -3 si autre
-*/
+    /*****************************************************************************************************
+     * RECEPTION D'UNE DEMANDE POUR S'EQUIPER OU SE DESEQUIPER D'UN ITEM
+     * return 1 si ok
+     * erreur : 0 si objet n'est pas dans le sac
+     * erreur : -1 si il y a déja une arme d'équipée
+     * erreur : -2  si il y a déja une armure d'équipée
+     * erreur : -3 si item n'est ni arme ni armure
+     * erreur : -4 si l'item a dequiper n'est pas équipé au préalable
+     */
+    
+    socket.on('INV_PERSONNAGE_CS', function(type, id_item)
+    		{
+    			// recupere l'currentItem
+    	        var currentItem = oItem_BD.GetItemById(id_item);
+    	        
+    	        // check si currentItem est bien dans le sac
+                var existItemInSac = myPerso.existItemInSac(currentItem);
+                if (existItemInSac == false)
+                	socket.emit('INV_PERSONNAGE_SC', 'EQUIPER', currentItem.id, 0);
+                
+    	        // si c'est une demande pour s'équiper
+    	        if (type == "EQUIPER")
+    	        	{
+    	        		// on équipe le perso 
+    	        		var reponse = myPerso.sEquiperDunItem(currentItem);
+    	        		// et selon le message renvoyé
+    	        		switch(reponse)
+    	        		{
+    	        			case 1 :  socket.emit('INV_PERSONNAGE_SC', 'EQUIPER', currentItem.id, 1); break;
+    	        			case -1 : socket.emit('INV_PERSONNAGE_SC', 'EQUIPER', currentItem.id, -1); break;
+    	        			case -2 : socket.emit('INV_PERSONNAGE_SC', 'EQUIPER', currentItem.id, -2);  break;
+    	        			case -3 : socket.emit('INV_PERSONNAGE_SC', 'EQUIPER', currentItem.id, -3); break;
+    	        			default :  break;
+    	        		}
+    	        	}
+    	        else if (type == "DEQUIPER")
+    	        	{
+    	        		// si le perso n'est pas équipe d'un item de cet idem
+    	        		if (myPerso.armeEquipee.id != currentItem.id
+    	        				|| myPerso.armureEquipee.id != currentItem.id)
+    	        			{
+    	        				socket.emit('INV_PERSONNAGE_SC', 'EQUIPER', currentItem.id, -4); 
+    	        			}
+    	        		var reponse = myPerso.sDesequiperDunItem(currentItem);
+    	        		socket.emit('INV_PERSONNAGE_SC', 'DEQUIPER', currentItem.id, 1);
+    	        	}
+    		});
+    /*****************************************************************************************************
+     * RECEPTION D'UNE DEMANDE POUR RAMASSER OU DEPOSER UN ITEM
+     * return poidsTotal si ok
+     * erreur : -1 si poids insufisant
+     * erreur : -2 si objet n'est pas dans la case / le sac
+     * erreur : -3 si autre
+     */
     socket.on('INV_CASE_CS', function (type, id_item) {
-        if (type == "RAMASSER") {
-            // récupère la salle en cours
-            var currentCase = oCase_BD.GetCaseById(myPerso.idSalleEnCours);
+    	// récupère la case en cours
+        var currentCase = oCase_BD.GetCaseById(myPerso.idSalleEnCours);
 
-            // recupere l'currentItem
-            var currentItem = oItem_BD.GetItemById(id_item);
-
+        // recupere l'currentItem
+        var currentItem = oItem_BD.GetItemById(id_item);
+        
+        if (type == "RAMASSER") 
+        {
             // log
             console.log("SERVER : Demande pour ramasser l'currentItem : " + id_item + " - " + currentItem.nom);
 
             // check si currentItem est bien dans la salle
-            var exist = currentCase.itemInSalle(currentItem);
+            var existItemInSalle = currentCase.existItemInSalle(currentItem);
 
             // si l'objet est bien dans la salle
-            if (exist == true) {
+            if (existItemInSalle == true) 
+            {
                 // check si l'objet peut être ajouté au personnage
                 console.log("SERVER : poids sac : " + myPerso.getPoidsSac() + " - poids item : " + currentItem.poids + " - poids max : " + myPerso.poidsMax);
                 if ((myPerso.getPoidsSac() + currentItem.poids) < myPerso.poidsMax) {
                     // ajout de l'currentItem au sac du perso
                     myPerso.ajouterAuSac(currentItem);
-                    // suppression de l'objet de la salle
-                    oCase_BD.SupprObjet(myPerso.idSalleEnCours, currentItem);
+                    // suppression de l'objet de la case
+                    currentCase.supprimerItem(currentItem);
                     // return au client
                     socket.emit('INV_CASE_SC', 'RAMASSER', currentItem.id, myPerso.getPoidsSac());
-                } else {
+                } 
+                else 
+                {
                     console.log("SERVER : Demande de ramassage impossible : poids max atteint");
                     // return au client que l'objet ne peut être ajouté (poids insufisant)
                     socket.emit('INV_CASE_SC', 'RAMASSER', currentItem.id, -1);
@@ -149,16 +182,50 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('INV_CASE_SC', 'RAMASSER', currentItem.id, -2);
             }
         }
+        else if (type == "DEPOSER")
+        {
+        	// log
+            console.log("SERVER : Demande pour deposer l'currentItem : " + id_item + " - " + currentItem.nom);
+
+            // check si currentItem est bien dans le sac
+            var existItemInSac = myPerso.existItemInSac(currentItem);
+
+            // si l'item est bien dans le sac
+            if (existItemInSac == true) 
+            {
+            	// ajout de l'item a la case
+            	currentCase.ajouterItem(currentItem);
+            	// suppression de l'item au perso
+            	myPerso.supprimerDuSac(currentItem);
+            	// return au client
+            	 socket.emit('INV_CASE_SC', 'DEPOSER', currentItem.id, myPerso.getPoidsSac());
+            }
+            // si l'item n'est pas dans le sac (! l'ihm n'a pas été mis à jour !)
+            else
+            	{
+            		 // return que l'item n'est pas dans le sac
+                    socket.emit('INV_CASE_SC', 'DEPOSER', currentItem.id, -2);
+            	}
+        }
+    });
+    /*****************************************************************************************************
+     * RECEPTION D'UNE DEMANDE D'INFOS SUR UNE CASE
+     * Renvoi la case avec INFO_CASE_SC
+     * Si erreur : renvoi NULL
+     */
+    socket.on('INFO_CASE_CS', function () {
+        // récupère la salle en cours
+        var currentCase = oCase_BD.GetCaseById(myPerso.idSalleEnCours);
+        // return selon la valeur de retour
+        if (currentCase == null)
+            socket.emit('INFO_CASE_SC', "ERREUR_CASE");
+        else
+            socket.emit('INFO_CASE_SC', currentCase);
     });
 
-
-    /*
-* RECEPTION D'UNE DEMANDE POUR RAMASSER OU DEPOSER UN OBJET
-* return poidsTotal si ok
-* erreur : -1 si poids insufisant
-* erreur : -2 si objet n'est pas dans la case
-* erreur : -3 si autre
-*/
+    /*****************************************************************************************************
+     * RECEPTION D'UNE DEMANDE D'INFO SUR LE PERSONNAGE
+     */
     socket.on('INFO_PERSONNAGE_CS', function () {
         socket.emit('INFO_PERSONNAGE_SC', myPerso);
     });
@@ -166,10 +233,12 @@ io.sockets.on('connection', function (socket) {
 });
 
 
+
+
 //server.listen(8080);
 server.on('close', function () { // On écoute l'évènement close
     console.log('Bye bye !');
 });
 
-console.log("Script lancé ! sur http://127.0.0.1:8080");
+console.log("SERVEUR : Script lancé ! sur http://127.0.0.1:8080");
 server.listen(8080);
