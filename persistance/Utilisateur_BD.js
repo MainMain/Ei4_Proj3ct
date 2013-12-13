c0184afec93e21e737530fd0002d3a155e248fef
Mainmain <psyko469@hotmail.fr>
2013-12-13 19:47:52 +0100

    Insription et connexion cote serveur ok

--- persistance/Utilisateur_BD.js
+++ persistance/Utilisateur_BD.js
@@ -2,7 +2,6 @@
 var oDatabase = require('../model/database');
 var mongoose = require('mongoose');
 
-
 /**
  * UTILISATEUR : COMMUNICATION SERVEUR <-> BD
  * 
@@ -14,16 +13,14 @@ function Utilisateur_BD() {
 	}
 };
 
-
 /**
  * ENVOI UN UTILISATEUR POUR METTRE A JOUR CES PROPRIETES
  * 
  * @method SetUtilisateur
  */
 Utilisateur_BD.SetUtilisateur = function(utilisateurToSave) {
-	
-},
 
+},
 
 /**
  * RENVOIE UN UTILISATEUR AVEC SON ID PASSE EN PARAMETRE
@@ -31,38 +28,43 @@ Utilisateur_BD.SetUtilisateur = function(utilisateurToSave) {
  * @method GetUtilisateur
  */
 Utilisateur_BD.GetUtilisateur = function(idUtilisateur) {
-	
+
 },
 
-
 /**
- * Ajoute un Utilisateur dans la base de donnée
- * 
+ * AJOUTE UN UTILISATEUR DANS LA BASE DE DONNEES
+ * Renvoi 1 si inscription ok
+ * Renvoi 0 si erreur
+ * Renvoi -1 si pseudo deja pris
+ * Renvoi -2 si email deja pris
  * @method Inscription
  */
- 
- Utilisateur_BD.Inscription = function(pseudoU,emailU,passU){
- 
- 
- 
+
+Utilisateur_BD.Inscription = function(pseudoU, emailU, passU) {
 	var Utilisateurmodel = mongoose.model('Utilisateur');
-	
+
 	var NewUser = new Utilisateurmodel();
-	
+
 	NewUser.pseudo = pseudoU;
 	NewUser.pass = passU;
 	NewUser.email = emailU;
-	
-	NewUser.save(function (err) {
-		if (err) { throw err; }
+
+	NewUser.save(function(err) {
+		if (err) {
+			throw err;
+		}
 		console.log('tu es dans la base maintenant Motherfuker !');
-	
 	});
-	
- },
 
- 
- module.exports = Utilisateur_BD;
+},
 
+/*
+ * DEMANDE DE CONNEXION D'UN UTILISATEUR
+ * Renvoi true si ok
+ * Renvoi false si couple inexistant
+ */
+Utilisateur_BD.Connexion = function(pseudoU, passU) {
+	// renvoi true si couple ok, false sinon
+},
 
-
+module.exports = Utilisateur_BD;
